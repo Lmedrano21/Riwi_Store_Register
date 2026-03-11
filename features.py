c4f67b4f6a5ba2b3f8b1b8229ca1475bf12a609b
Nelson-B26 <nelbalvin2005Qgmail.com>
2026-03-11 15:48:19 -0500

    Segundo avance

---
+++ features.py
@@ -0,0 +1,48 @@
+
+
+def record_sales2():
+    print("Sales recorded successfully.")
+    record_sales = {}
+    producto_name = input("Enter the name of the product: ")
+    Producto_price = float(input("Enter the price of the product: "))
+    quantity = int(input("Enter the quantity sold: "))
+    
+    total_sale = Producto_price * quantity
+    record_sales[producto_name] = {
+        "price": Producto_price,
+        "quantity": quantity,
+        "total_sale": total_sale
+    }
+    return record_sales
+
+
+
+
+def record_sales1():
+    validate_record = True
+    print("Welcome to the sales recording system.")
+    total_records = {}
+    while validate_record:
+        decision = input("Do you want to record a sale? (yes/no): ").strip().lower()
+        if decision == 'no':
+            total_records_day = sum(details['total_sale'] for details in total_records.values())
+            print("Thank you for using the sales recording system.")
+            print("Here are the recorded sales:")
+            for product, details in total_records.items():
+                print(f"Product: {product}")
+                print(f"  Price: {details['price']}")
+                print(f"  Quantity: {details['quantity']}")
+                print(f"  Total Sale: {details['total_sale']}")
+                print(f"Total sales recorded for the day: {total_records_day}")
+            print(f"Total sales recorded: {total_records}")
+            print("Exiting the sales recording system.")
+            
+            validate_record = False
+        elif decision == 'yes':
+            total_records.update(record_sales2())
+            print("Sales recorded successfully.")
+            print
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+    return
+record_sales1() 
